0a79e85fea00d711efbc7180809aa1fd600c24c6
JhoniSanchez <jhonimanuel7184.js@gmail.com>
2025-05-23 16:54:17 -0400

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/paga-ginita/", // Reemplaza <repositorio> con el nombre de tu repositorio
+  base: "paga-ginita", // Reemplaza <repositorio> con el nombre de tu repositorio
 })
